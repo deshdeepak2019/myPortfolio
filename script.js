e112144bf63a69ebb949f730c5e9f23b78306009
Desh Deepak Dhruv <97728256+deshdeepak2019@users.noreply.github.com>
2024-01-17 15:17:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Competitive Coder", "Designer", "Teacher"],
+        strings: ["Software Developer", "Competitive Coder"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Competitive Coder", "Designer", "Teacher"],
+        strings: ["Software Developer", "Competitive Coder"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
